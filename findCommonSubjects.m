f3c54424719d73b52e00797b05d77400864ad1b8
albertogastaldello <alberto.gastaldello.2@studenti.unipd.it>
2025-12-12 15:41:48 +0100

    find common subjects
    find and save subjects common to basal, bolus, cgm and roster data

---
+++ findCommonSubjects.m
@@ -0,0 +1,19 @@
+clc
+clear all
+close all
+
+%% find subjects ID in common with all data and save them
+
+roster = readtable("Data Tables/PtRoster.txt");
+
+rosterSubjID = roster.PtID;
+cgmSubjID = load("cgm_subjectsID_numeric.mat").cgm_subjectsID_numeric;
+basalSubjID = load("basal_subjectsID_numeric.mat").basal_subjectsID_numeric;
+bolusSubjID = load("bolus_subjectsID_numeric.mat").bolus_subjectsID_numeric;
+
+common_subjectsID_numeric = intersect(intersect(intersect(rosterSubjID, cgmSubjID), basalSubjID), bolusSubjID);
+common_subjectsID = string(common_subjectsID_numeric);
+
+save("common_subjectsID_numeric", "common_subjectsID_numeric");
+save("common_subjectsID", "common_subjectsID");
+
