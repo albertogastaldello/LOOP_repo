9144f75a625181da319d9c29ba6849196c0bed67
albertogastaldello <alberto.gastaldello.2@studenti.unipd.it>
2025-12-17 15:40:42 +0100

    re defined common subjects
    common subjects are those who completed the study, have cgm, basal and bolus data, have hba1c data, have baseline data

--- findCommonSubjects.m
+++ findCommonSubjects.m
@@ -6,16 +6,20 @@ close all
 
 roster = readtable("Data Tables/PtRoster.txt");
 surveys = readtable("Data Tables/Surveys.txt");
+sampleResults = readtable("Data Tables/SampleResults.txt");
 
 rosterSubjID = roster.PtID;
+rosterCompleted = roster(strcmp(roster.PtStatus, 'Completed'),:);
+rosterCompletedSubjID = rosterCompleted.PtID;
+
 surveysID = unique(surveys.SubjectID);
 cgmSubjID = load("cgm_subjectsID_numeric.mat").cgm_subjectsID_numeric;
 basalSubjID = load("basal_subjectsID_numeric.mat").basal_subjectsID_numeric;
 bolusSubjID = load("bolus_subjectsID_numeric.mat").bolus_subjectsID_numeric;
+sampleResultsSubjID = unique(sampleResults.PtID);
 
-common_subjectsID_numeric = intersect(intersect(intersect(intersect(rosterSubjID, cgmSubjID), basalSubjID), bolusSubjID), surveysID);
+common_subjectsID_numeric = intersect(intersect(intersect(intersect(rosterCompletedSubjID, cgmSubjID), basalSubjID), sampleResultsSubjID), surveysID);
 common_subjectsID = string(common_subjectsID_numeric);
 
 save("common_subjectsID_numeric.mat", "common_subjectsID_numeric");
 save("common_subjectsID.mat", "common_subjectsID");
-
